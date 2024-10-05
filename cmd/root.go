a576555a4d21cad5b17320785e33af4da166e09f
Leonel Higuera <lhiguer1@asu.edu>
2024-10-05 01:01:07 -0700

    update root command help message.

--- cmd/root.go
+++ cmd/root.go
@@ -12,16 +12,7 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "tasker",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Task tracker CLI app to track your tasks and manage your to-do list.",
 }
 
 var tasksFile string
